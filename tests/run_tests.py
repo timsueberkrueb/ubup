811082d15cb000122ff012e8056c30fb5c8f328e
Tim Süberkrüb <dev@timsueberkrueb.io>
2018-01-20 00:25:28 +0100

    Support to run tests in Docker Build a custom Docker image to get snapd working in Docker based on: https://github.com/ogra1/snapd-docker/

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,6 +4,7 @@
 import os
 import subprocess
 import time
+import tempfile
 
 import click
 
@@ -18,20 +19,114 @@ SUPPORTED_UBUNTU_RELEASES = (
 )
 
 
-def _check_is_lxc_installed():
+_HYPERVISOR_MISSING_MESSAGE = '''LXD or Docker are required in order to run tests without polluting the host system.
+Switch between hypervisors using the --docker or --lxd command line options.
+Install LXD it by running:
+$ snap install lxd
+and set it up by running:
+$ lxd init
+Install Docker by running:
+$ snap install docker
+'''
+
+
+def _check_is_lxd_installed():
     try:
         subprocess.check_call(['lxd', '--version'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        subprocess.check_call(['lxc', '--version'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     except FileNotFoundError:
         raise click.ClickException('LXD doesn\'t seem to be installed on your system or is not in $PATH.\n'
-                                   'LXD is required to run tests without polluting the host system.\n'
-                                   'Install it by running:\n'
-                                   '$ snap install lxd\n'
-                                   'and set it up by running:\n'
-                                   '$ lxd init')
+                                   + _HYPERVISOR_MISSING_MESSAGE)
 
 
-def _wait_for_network(container_name):
+def _check_is_docker_installed():
+    try:
+        subprocess.check_call(['docker', '--version'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except FileNotFoundError:
+        raise click.ClickException('Docker doesn\'t seem to be installed on your system or is not in $PATH.\n'
+                                   + _HYPERVISOR_MISSING_MESSAGE)
+
+
+def _docker_wait_for_snapd(container_name):
+    print('Waiting for snapd to start up ...')
+    ready = False
+    retry_count = 25
+    probe = ['docker', 'exec', container_name, 'pgrep', 'snapd']
+    while not ready:
+        time.sleep(1)
+        try:
+            result = subprocess.check_call(probe)
+            ready = result == 0
+        except subprocess.CalledProcessError:
+            ready = False
+            retry_count -= 1
+            if retry_count == 0:
+                raise ConnectionError("Error: snapd not running.")
+    print('Snapd is up and running.')
+
+
+def _run_in_docker(verbose: bool=False):
+    for release in SUPPORTED_UBUNTU_RELEASES:
+        run_tests_command = ['bash', '-c', 'export LC_ALL=C.UTF-8 && export LANG=C.UTF-8 '
+                                           '&& cd /root/ubup '
+                                           '&& python3 ./tests/run_tests.py --perform-on-host'
+                                           + (' --verbose' if verbose else '')]
+        container_name = 'ubup-tests-runner-{}'.format(release)
+
+        # Source: https://github.com/ogra1/snapd-docker
+        subprocess.check_call([
+            'docker', 'run', '--name={}'.format(container_name), '-it', '--tmpfs', '/run', '--tmpfs', '/run/lock',
+            '--tmpfs', '/tmp', '--cap-add', 'SYS_ADMIN', '--device=/dev/fuse', '--security-opt', 'apparmor:unconfined',
+            '--security-opt', 'seccomp:unconfined', '-v', '/sys/fs/cgroup:/sys/fs/cgroup:ro',
+            '-v', '/lib/modules:/lib/modules:ro', '-d', 'ubup-snapd-ubuntu-{}'.format(release)
+        ])
+        subprocess.check_call(['docker', 'start', container_name])
+
+        try:
+            # Wait for snapd to start up
+            _docker_wait_for_snapd(container_name)
+            # Edge core snap currently required
+            subprocess.check_call(['docker', 'exec', '-i', container_name, 'snap', 'install', 'core', '--edge'])
+            # Push the source code to the container
+            subprocess.check_call(['docker', 'cp', SOURCE_ROOT, container_name + ':/root'])
+            # Update and upgrade our fresh container
+            subprocess.check_call(['docker', 'exec', '-i', container_name, 'apt-get', 'update'])
+            subprocess.check_call(['docker', 'exec', '-i', container_name, 'apt-get', '-y', 'upgrade'])
+            # Install software-properties-common (for add-apt-repository)
+            # which is not installed by default in the Ubuntu Docker images.
+            subprocess.check_call(['docker', 'exec', '-i', container_name,
+                                   'apt-get', '-y', 'install', 'software-properties-common'])
+            # Install Python and pip
+            subprocess.check_call(['docker', 'exec', '-i', container_name,
+                                   'apt-get', '-y', 'install', 'python3', 'python3-pip'])
+            # Install ubup dependencies
+            subprocess.check_call(['docker', 'exec', '-i', container_name,
+                                   'pip3', 'install', 'click', 'ruamel.yaml', 'schema', 'progressbar2'])
+            # Install test requirements
+            subprocess.check_call(['docker', 'exec', '-i', container_name, 'pip3', 'install', 'pytest'])
+            # Run the test suite in the container
+            subprocess.check_call(['docker', 'exec', '-i', container_name, *run_tests_command])
+            # Delete the container
+            subprocess.check_call(['docker', 'rm', '-f', container_name])
+        except subprocess.CalledProcessError as e:
+            print('An error occurred, trying to destroy container ...')
+            subprocess.check_call(['docker', 'rm', '-f', container_name])
+            print('Container destroyed.')
+            raise e
+
+
+def _build_docker_images():
+    dockerfile_in = os.path.join(TESTS_DIR, 'docker', 'Dockerfile.in')
+    with open(dockerfile_in) as file:
+        dockerfile_text = file.read()
+    for release in SUPPORTED_UBUNTU_RELEASES:
+        with tempfile.TemporaryDirectory() as tempdir:
+            with open(os.path.join(tempdir, 'Dockerfile'), 'w') as file:
+                file.write(dockerfile_text.replace('$RELEASE', release))
+            subprocess.check_call(['docker', 'build', '-t', 'ubup-snapd-ubuntu-{}'.format(release), '.'],
+                                  cwd=os.path.join(tempdir))
+
+
+def _lxc_wait_for_network(container_name):
     print('Waiting for a network connection ...')
     connected = False
     retry_count = 25
@@ -55,28 +150,29 @@ def _wait_for_network(container_name):
 
 def _run_in_lxc_container(verbose: bool=False):
     for release in SUPPORTED_UBUNTU_RELEASES:
-        run_tests_command = ['bash', '-c', 'cd /root/ubup '
+        run_tests_command = ['bash', '-c', 'export LC_ALL=C.UTF-8 && export LANG=C.UTF-8 '
+                                           '&& cd /root/ubup '
                                            '&& python3 ./tests/run_tests.py --perform-on-host'
-                                           + ' --verbose' if verbose else '']
+                                           + (' --verbose' if verbose else '')]
         container_image = 'ubuntu:{}'.format(release)
         container_name = 'ubup-tests-runner-{}'.format(release)
         subprocess.check_call(['lxc', 'launch', '-e', container_image, container_name])
         try:
             # Push the source code to the container
             subprocess.check_call(['lxc', 'file', 'push', '-r', SOURCE_ROOT, container_name + '/root'])
-            _wait_for_network(container_name)
+            _lxc_wait_for_network(container_name)
             # Update and upgrade our fresh container
             subprocess.check_call(['lxc', 'exec', container_name, '--', 'apt-get', 'update'])
             subprocess.check_call(['lxc', 'exec', container_name, '--', 'apt-get', '-y', 'upgrade'])
+            # Install squashfs (required for snaps to work)
+            subprocess.check_call(['lxc', 'exec', container_name, '--',
+                                   'apt-get', '-y', 'install', 'squashfuse'])
             # Install Python and pip
             subprocess.check_call(['lxc', 'exec', container_name, '--',
                                    'apt-get', '-y', 'install', 'python3', 'python3-pip'])
             # Install ubup dependencies
             subprocess.check_call(['lxc', 'exec', container_name, '--',
                                    'pip3', 'install', 'click', 'ruamel.yaml', 'schema', 'progressbar2'])
-            # Install squashfs (required for snaps to work)
-            subprocess.check_call(['lxc', 'exec', container_name, '--',
-                                   'apt-get', 'install', 'squashfuse'])
             # Install test requirements
             subprocess.check_call(['lxc', 'exec', container_name, '--', 'pip3', 'install', 'pytest'])
             # Run the test suite in the container
@@ -101,14 +197,22 @@ def _run_on_host(verbose: bool=False):
 
 @click.command()
 @click.option('--verbose', default=False, is_flag=True, help='Enable verbose output.')
+@click.option('--docker/--lxd', default=False, is_flag=True, help='Container hypervisor to use')
+@click.option('--build-docker-images', default=False, is_flag=True, help='Build Docker images')
 @click.option('--perform-on-host', default=False, is_flag=True,
               help='Perform tests on the host rather than inside of a container. '
                    'You should not invoke this manually.')
-def main(verbose: bool=False, perform_on_host: bool=False):
+def main(verbose: bool=False, docker: bool=False, build_docker_images: bool=False, perform_on_host: bool=False):
+    if build_docker_images:
+        _check_is_docker_installed()
+        _build_docker_images()
     if perform_on_host:
         _run_on_host(verbose)
+    elif docker:
+        _check_is_docker_installed()
+        _run_in_docker(verbose)
     else:
-        _check_is_lxc_installed()
+        _check_is_lxd_installed()
         _run_in_lxc_container(verbose)
 
 
